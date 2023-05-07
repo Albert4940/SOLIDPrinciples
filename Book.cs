92e001bf9ccca402ca3a7db7b986cef54773551f
Albert4940 <dorcealbertmary@gmail.com>
2023-05-07 00:34:17 -0500

    Ajoutez des fichiers projet.

---
+++ Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipleS
+{
+    public class Book
+    {
+        public string Title { get; set; } = "";
+        public string Author { get; set; } = "";
+        public int NbPages { get; set; } = 0;
+
+        public string ISBN { get; set; } = "";
+
+       
+    }
+}
---
+++ BookRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PrincipleS
+{
+    public class BookRepository
+    {
+        public async Task SaveToFile(Book book)
+        {
+            await File.WriteAllTextAsync($"./book-{book.Title}.json", JsonSerializer.Serialize(book, new
+                JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            }
+                ));
+        }
+    }
+}
