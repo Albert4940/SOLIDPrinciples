0164db9bc7b3acaf80afabe4c91ed338608d4e1c
Albert4940 <dorcealbertmary@gmail.com>
2023-05-17 11:55:47 -0500

    PrincipleO

---
+++ PrincipleO/Rectangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipleO
+{
+    public class Rectangle : Shape
+    {
+        public int Height { set; get; }
+        public int Width { set; get; }
+
+        public override int Surface => Height + Width;
+    }
+}
---
+++ PrincipleO/SurfaceCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipleO
+{
+    public class SurfaceCalculator
+    {
+        public int ComputeAllSizes(IEnumerable<Shape> shapes)
+        {
+            int result = 0;
+            foreach(var shape in shapes)
+            {
+                result += shape.Surface;
+            }
+            return result;
+        }
+
+        /*private int ComputeSize(Shape shape)
+        {
+            if(shape is Square s)
+            {
+                return s.SideSize * s.SideSize;
+            }
+            else if(shape is Rectangle r)
+            {
+                return r.Height + r.Width;
+            }
+            throw new InvalidOperationException();
+        }*/
+    }
+}
